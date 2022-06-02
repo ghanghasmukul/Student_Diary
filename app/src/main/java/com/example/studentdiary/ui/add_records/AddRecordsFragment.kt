4999fb09fb7dd4340d44f7953b10e67c8ebec798
mukul <mukulghanghas1@gmail.com>
2022-06-02 18:50:06 +0530

    added Exo Player in PlayVideosFragment.kt

--- app/src/main/java/com/example/studentdiary/ui/add_records/AddRecordsFragment.kt
+++ app/src/main/java/com/example/studentdiary/ui/add_records/AddRecordsFragment.kt
@@ -1,18 +1,38 @@
 package com.example.studentdiary.ui.add_records
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.view.isEmpty
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.studentdiary.databinding.FragmentAddRecordsBinding
 import com.example.studentdiary.roomdatabase.StudentDetails
 import com.example.studentdiary.roomdatabase.StudentViewModel
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.GoogleMap
+
+import java.util.*
+
 
 class AddRecordsFragment : Fragment() {
+    val REQUEST_LOCATION = 100
+    private var googleMap: GoogleMap? = null
+    private val permissionCode = 101
+    private var currentLocation: Location? = null
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private var locationManager: LocationManager? = null
+    private var gpsEnabled: Boolean = false
+    private var networkEnabled: Boolean = false
+
     private lateinit var studentList: ArrayList<StudentDetails>
     private var studentViewModel: StudentViewModel = StudentViewModel()
 
@@ -32,25 +52,29 @@ class AddRecordsFragment : Fragment() {
 
         _binding = FragmentAddRecordsBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(requireContext())
 
-        val name = binding.enterNameET.editText
-        val roll_no = binding.rollNoET.editText
-        val phone_no = binding.phoneNoET.editText
+        onSubmitClick()
 
 
+        return root
+    }
 
+
+    private fun onSubmitClick() {
         binding.submitBtn.setOnClickListener {
 
-            val name = name?.text.toString()
-            val roll_no = roll_no?.text.toString()
-            val phone_no = phone_no?.text.toString()
+
+            val name = binding.enterNameET.editText?.text.toString()
+            val roll_no = binding.rollNoET.editText?.text.toString()
+            val phone_no = binding.phoneNoET.editText?.text.toString()
             if (name.isEmpty() || phone_no.isEmpty() || roll_no.isEmpty()) {
                 if (name.isEmpty()) {
                     binding.enterNameET.error = "Name can't be empty"
                     Toast.makeText(requireContext(), "empty name", Toast.LENGTH_SHORT).show()
                     binding.enterNameET.isErrorEnabled = true
-                }
-                else{
+                } else {
                     binding.enterNameET.error = null
                     binding.enterNameET.isErrorEnabled = false
 
@@ -60,8 +84,7 @@ class AddRecordsFragment : Fragment() {
 
                     binding.rollNoET.error = "Roll no. can't be empty"
                     binding.enterNameET.isErrorEnabled = true
-                }
-                else{
+                } else {
                     binding.rollNoET.error = null
                     binding.rollNoET.isErrorEnabled = false
                 }
@@ -69,8 +92,7 @@ class AddRecordsFragment : Fragment() {
                     Toast.makeText(requireContext(), "empty phone", Toast.LENGTH_SHORT).show()
                     binding.phoneNoET.error = "Phone no. can't be empty"
                     binding.enterNameET.isErrorEnabled = true
-                }
-                else{
+                } else {
                     binding.phoneNoET.error = null
                     binding.phoneNoET.isErrorEnabled = false
                 }
@@ -81,11 +103,7 @@ class AddRecordsFragment : Fragment() {
                     StudentDetails(name, roll_no, phone_no)
                 )
             }
-
         }
-
-
-        return root
     }
 
     fun setData(userList: ArrayList<StudentDetails>) {
@@ -93,8 +111,12 @@ class AddRecordsFragment : Fragment() {
 
     }
 
+
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+
 }
